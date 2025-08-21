fd35c97b8de2e50463a8443c7c0c3b10d2496a08
Gilbert Engalan <gilbertpude2003@gmail.com>
2025-08-21 15:34:23 +0800

    fix(header): fixed the error menu list behave function

--- web/src/components/Header.jsx
+++ web/src/components/Header.jsx
@@ -96,6 +96,7 @@ const Header = () => {
                         // alert("user log-out success");
                         toast.success("User logged out")
                         // window.location.href = "/"; // redirect
+                        setIsUserMenuOpen(false); 
                         navigate("/")
                       }}
                       className="block w-full text-left px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:hover:bg-gray-700"
